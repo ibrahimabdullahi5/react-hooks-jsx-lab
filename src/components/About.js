02905b1585d81f87b551effb78d33364fcda947b
ibrahimabdullahibare <ibrahimabdullahibare@gail.com>
2024-09-25 22:41:30 -0900

    react-hooks-jsx-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js"; 
 
-function About() {
-  return <div>About</div>;
-}
+const About = () => {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a passionate web developer with a love for creating dynamic and responsive websites.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+};
 
 export default About;
+
